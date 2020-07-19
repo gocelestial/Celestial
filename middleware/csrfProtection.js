ce5a636ee7f08db6ff65f9c395c62e02c5ba8882
hirusi <hello@rusingh.com>
2020-07-19 12:16:10 +0530

    Remove dist files from git index

--- middleware/csrfProtection.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.csrfProtection = void 0;
-var csrf = require("csurf");
-// Middleware to generate a CSRF token and make it available on req object
-var csrfProtection = csrf({ cookie: false });
-exports.csrfProtection = csrfProtection;
