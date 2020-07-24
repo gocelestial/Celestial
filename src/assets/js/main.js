1faa042d40e908d3d4033eac8a1dfee9aa87368d
hirusi <inbox@rusingh.com>
2020-07-24 22:16:31 +0530

    Implement a user menu and a user profile page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,11 @@
 import { emoji } from "./emoji";
-import { activeNavigation } from "./navigation";
+import { userMenu, activeNavigation } from "./navigation";
 import { setTimezone } from "./forms";
 
 window.addEventListener("DOMContentLoaded", () => {
 	emoji();
 	activeNavigation();
+	userMenu();
 
 	if (document.querySelectorAll("form").length) {
 		// TODO Only used on login form
--- src/assets/js/navigation.js
+++ src/assets/js/navigation.js
@@ -1,3 +1,5 @@
+const _debounce = require("lodash.debounce");
+
 const activeNavigation = () => {
 	const links = document.querySelectorAll(".navigation__link");
 	Array.prototype.forEach.call(links, (link) => {
@@ -6,4 +8,38 @@ const activeNavigation = () => {
 	});
 };
 
-export { activeNavigation };
+const userMenu = () => {
+	const menuToggle = document.querySelector(".page-header__user-identity");
+
+	const eventHandler = () =>
+		document
+			.querySelector(".user-menu")
+			.classList.toggle("user-menu--active");
+
+	["click", "keyup"].forEach((eventType) =>
+		menuToggle.addEventListener(
+			eventType,
+			(event) => {
+				if (event.type === "keyup" && event.code !== "Enter") return;
+				if (event.type === "keyup") _debounce(eventHandler, 600);
+				else eventHandler();
+			},
+			{ passive: true }
+		)
+	);
+
+	menuToggle.addEventListener(
+		"blur",
+		(event) => {
+			// Many browsers implement a 300-350ms delay for registering a single click
+			window.setTimeout(() => {
+				document
+					.querySelector(".user-menu")
+					.classList.remove("user-menu--active");
+			}, 351);
+		},
+		{ passive: true }
+	);
+};
+
+export { userMenu, activeNavigation };
