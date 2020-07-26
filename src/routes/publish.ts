6649c6ed437fcc4d6d03c4b7eddd403aaf7aca72
hirusi <inbox@rusingh.com>
2020-07-26 10:29:21 +0530

    fix: remove unnecessary check for login on publishing routes

--- src/routes/publish.ts
+++ src/routes/publish.ts
@@ -5,33 +5,23 @@ import {
 	NextFunction as ExpressNextFunction,
 } from "express";
 import fetch from "node-fetch";
-import { URLSearchParams } from "url";
 import { DateTime } from "luxon";
+import getType from "post-type-discovery";
 
 // Our interface, enums, middleware, libs
 import { PostPageData } from "../interface/PageData";
 
 import { urlEncodedParser } from "../middleware/urlEncodedParser";
 
+import { LogLevels } from "../enumerator/LogLevels";
+import { FormEncoding } from "../enumerator/FormEncoding";
+
 import { pageDataHelper } from "../lib/helpers";
 import { resetEphemeralSessionData } from "../lib/session";
 import { logger } from "../lib/logger";
-import { LogLevels } from "../enumerator/LogLevels";
+import { prepareParams } from "../lib/publish";
 
 const publishRouter = Router();
-
-// Validate session exists or redirect to login
-publishRouter.use(
-	(req: ExpressRequest, res: ExpressResponse, next: ExpressNextFunction) => {
-		if (req.session && !req.session.isLoggedIn) {
-			next({
-				code: "AppError",
-				message: "You need to login first.",
-			});
-		}
-		next();
-	}
-);
 
 publishRouter.get("/success/", (req: ExpressRequest, res: ExpressResponse) => {
 	const pageData: PostPageData = pageDataHelper(req, {
@@ -73,116 +63,6 @@ publishRouter.get("/note/", (req: ExpressRequest, res: ExpressResponse) => {
 
 	res.render("publish/note", pageData);
 });
-
-publishRouter.post(
-	"/note/",
-	urlEncodedParser,
-	(req: ExpressRequest, res: ExpressResponse, next: ExpressNextFunction) => {
-		logger.log(LogLevels.http, "Received a publishing request.", {
-			body: req.body,
-		});
-		// Make a POST url encoded request to the micropub endpoint, along with the access token
-		// The date and time we receive are in user's tz
-		// Looking at the source code of Indiekit, we'd like to send the date as an ISO8601 date (any timezone will work as they format it to UTC ultimately - but we'll send it in UTC for hopefully greater interoperability)
-		const [year, month, day] = req.body.date.split("-");
-		const [hour, minute] = req.body.time.split(":");
-		const published = DateTime.fromObject({
-			year,
-			month,
-			day,
-			hour,
-			minute,
-			zone: req.session?.user?.timezone,
-		})
-			.toUTC()
-			.toISO();
-
-		const params = new URLSearchParams();
-		params.append("h", req.body.h);
-		params.append("content", req.body.note);
-		params.append("published", published.toString());
-
-		// Can be a string, an emtpy string, or an array of string
-		if (
-			req.body?.["mp-syndicate-to"] &&
-			req.body["mp-syndicate-to"] !== ""
-		) {
-			if (Array.isArray(req.body["mp-syndicate-to"])) {
-				// Is an array
-				for (const target of req.body["mp-syndicate-to"]) {
-					params.append("mp-syndicate-to[]", target);
-				}
-			} else {
-				// Is a string
-				params.append("mp-syndicate-to", req.body["mp-syndicate-to"]);
-			}
-		}
-
-		logger.log(
-			LogLevels.http,
-			"Sending publish request to your Micropub server.",
-			{
-				h: params.get("h"),
-				content: params.get("content"),
-				published: params.get("published"),
-				"mp-syndicate-to": params.getAll("mp-syndicate-to[]"),
-			}
-		);
-
-		fetch(req.session?.endpoints?.micropub, {
-			method: "POST",
-			headers: {
-				Accept: "application/json",
-				Authorization: `Bearer ${req.session?.indieauth?.access_token}`,
-			},
-			body: params,
-		})
-			.then((response) => {
-				// TODO Handle a 202 response. We expect a 201 response later if we receive a 202.
-				if (!response.ok) {
-					response
-						.json()
-						.then((responseData) => {
-							logger.log(
-								LogLevels.error,
-								"Received an error from the Micropub server.",
-								{ error: responseData }
-							);
-							next({
-								code: "MicropubServerError",
-								message: `${responseData.error} ${responseData?.error_description}`,
-							});
-						})
-						.catch((error) => {
-							logger.log(
-								LogLevels.error,
-								"Received an error from the Micropub server but could not read it.",
-								{ error }
-							);
-							throw new Error(error);
-						});
-				}
-
-				logger.log(
-					LogLevels.http,
-					"Received a response from the Micropub server",
-					{ response }
-				);
-				if (response.headers.get("Location"))
-					if (req.session)
-						req.session.postLink = response.headers.get("Location");
-				// TODO Set shortlinks and syndication links, show them to the user
-				// https://www.w3.org/TR/2017/REC-micropub-20170523/#response-p-5
-				res.redirect(302, "/publish/success/");
-			})
-			.catch((error: Error | TypeError) => {
-				next({
-					code: "AppError",
-					message: error.message,
-				});
-			});
-	}
-);
 
 publishRouter.get("/reply/", (req: ExpressRequest, res: ExpressResponse) => {
 	const pageData: PostPageData = pageDataHelper(req, {
@@ -263,5 +143,91 @@ publishRouter.get("/rsvp/", (req: ExpressRequest, res: ExpressResponse) => {
 
 	res.render("publish/rsvp", pageData);
 });
+
+// Common publishing endpoint
+publishRouter.post(
+	"/publish/",
+	urlEncodedParser,
+	(req: ExpressRequest, res: ExpressResponse, next: ExpressNextFunction) => {
+		logger.log(LogLevels.http, "Received a publishing request.", {
+			body: req.body,
+		});
+
+		let requestOptions: object;
+		switch (req.session?.user?.preferences?.publishingType) {
+			case FormEncoding.URLEncoded:
+			default:
+				const params = prepareParams(req);
+
+				req.body;
+
+				logger.log(
+					LogLevels.verbose,
+					"Sending publish request to your Micropub server.",
+					{
+						encoding: FormEncoding.URLEncoded,
+						h: params.get("h"),
+						content: params.get("content"),
+						published: params.get("published"),
+						"mp-syndicate-to": params.getAll("mp-syndicate-to[]"),
+					}
+				);
+
+				requestOptions = {
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						Authorization: `Bearer ${req.session?.indieauth?.access_token}`,
+					},
+					body: params,
+				};
+		}
+
+		fetch(req.session?.endpoints?.micropub, requestOptions)
+			.then((response) => {
+				if (!response.ok) {
+					response
+						.json()
+						.then((responseData) => {
+							logger.log(
+								LogLevels.error,
+								"Received an error from the Micropub server.",
+								{ error: responseData }
+							);
+							next({
+								code: "MicropubServerError",
+								message: `${responseData.error} ${responseData?.error_description}`,
+							});
+						})
+						.catch((error) => {
+							logger.log(
+								LogLevels.error,
+								"Received an error from the Micropub server but could not read it.",
+								{ error }
+							);
+							throw new Error(error);
+						});
+				}
+
+				logger.log(
+					LogLevels.http,
+					"Received a response from the Micropub server",
+					{ response }
+				);
+				if (response.headers.get("Location"))
+					if (req.session)
+						req.session.postLink = response.headers.get("Location");
+				// TODO Set shortlinks and syndication links, show them to the user
+				// https://www.w3.org/TR/2017/REC-micropub-20170523/#response-p-5
+				res.redirect(302, "/publish/success/");
+			})
+			.catch((error: Error | TypeError) => {
+				next({
+					code: "AppError",
+					message: error.message,
+				});
+			});
+	}
+);
 
 export { publishRouter };
